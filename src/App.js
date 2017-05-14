671b6cbe0bca6a800b98b4db95ecbb1422c57d15
Guilherme Kodama <guilherme.kodama@clevertech.biz>
2017-05-14 18:10:11 -0400

    feat(comments): As a user I want to be able to view comments belonging to a post (#7)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import AppBar from 'material-ui/AppBar';
 import {Card, CardHeader, CardText} from 'material-ui/Card'
+import {List, ListItem} from 'material-ui/List'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import axios from 'axios'
@@ -8,11 +10,48 @@ import axios from 'axios'
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin()
 
+class Comment extends Component {
+  render() {
+    const { comment } = this.props
+    return (
+      <Card className='card'>
+        <CardHeader title={comment.name} />
+        <CardText> {comment.body} </CardText>
+      </Card>
+    )
+  }
+}
+
 class CardExpandable extends Component {
+  constructor() {
+    super()
+    this.state = {
+      comments: []
+    }
+  }
+
+  getComments(post) {
+    this.props.setTitle(post)
+    axios.get(`http://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
+      .then(res => {
+        const comments = res.data
+        console.log(comments.length)
+        this.setState({
+          comments
+        })
+      })
+  }
+
+  renderComment(comment, i) {
+    return (
+      <ListItem key={comment.id}> <Comment comment={comment}/> </ListItem>
+    )
+  }
+
   render() {
     const { post } =  this.props
     return (
-      <Card className='card'>
+      <Card className='card' onExpandChange={() => this.getComments(post)}>
         <CardHeader
           title={post.title}
           subtitle='Subtitle'
@@ -20,6 +59,9 @@ class CardExpandable extends Component {
           showExpandableButton={true}
         />
         <CardText expandable={true}> {post.body} </CardText>
+        <List expandable={true}>
+          {this.state.comments.map(this.renderComment)}
+        </List>
       </Card>
     )
   }
@@ -29,30 +71,33 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
+      title: 'Title',
       posts: []
     }
+  }
+
+  setTitle(post) {
+    this.setState({ title: post.title, posts: this.state.posts })
   }
 
   componentDidMount() {
     axios.get('http://jsonplaceholder.typicode.com/posts')
       .then(res => {
-        console.log(res.data[0])
         const posts = res.data
-        this.setState({ posts })
+        this.setState({ title: this.state.title, posts })
       })
-  }
-
-  renderPost(post, i) {
-    return (
-      <CardExpandable post={post} key={post.id}/>
-    )
   }
 
   render() {
     return (
       <MuiThemeProvider>
         <div>
-          {this.state.posts.map(this.renderPost)}
+          <div>
+            <AppBar title={this.state.title} iconClassNameRight="muidocs-icon-navigation-expand-more"/>
+          </div>
+          <div>
+            {this.state.posts.map((post, i) => <CardExpandable post={post} key={post.id} setTitle={this.setTitle.bind(this)}/> )}
+          </div>
         </div>
       </MuiThemeProvider>
     );
