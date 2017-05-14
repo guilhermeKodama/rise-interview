2f90030bc9a38d85cb0a787a15cad9280120228b
Guilherme Kodama <guilherme.kodama@clevertech.biz>
2017-05-14 16:39:02 -0400

    feat(posts): As a user I want to be able to view all posts (#6)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Card, CardHeader, CardText} from 'material-ui/Card'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import axios from 'axios'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
+class CardExpandable extends Component {
+  render() {
+    const { post } =  this.props
+    return (
+      <Card className='card'>
+        <CardHeader
+          title={post.title}
+          subtitle='Subtitle'
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+        <CardText expandable={true}> {post.body} </CardText>
+      </Card>
+    )
+  }
+}
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        console.log(res.data[0])
+        const posts = res.data
+        this.setState({ posts })
+      })
+  }
+
+  renderPost(post, i) {
+    return (
+      <CardExpandable post={post} key={post.id}/>
+    )
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div>
+          {this.state.posts.map(this.renderPost)}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
